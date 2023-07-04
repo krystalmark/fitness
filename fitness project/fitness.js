09abef3e75515e8fccbc5d7b07a2a7e81bbf4c5c
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-07-04 23:55:10 +0300

    exporting and importing from javascript files

---
+++ fitness project/fitness.js
@@ -0,0 +1,3 @@
+export default class FitnessApp{
+   constructor(root) 
+}
---
+++ fitness project/index.js
@@ -0,0 +1,4 @@
+import FitnessApp  from "./fitness.js";
+
+const app = document.getElementById("app");
+new FitnessApp(app);
