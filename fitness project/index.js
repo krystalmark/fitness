6aeb01b66f7df1425f3a9693c8e4d032a25c3468
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-07-05 02:32:02 +0300

    displaying data

--- fitness project/index.js
+++ fitness project/index.js
@@ -1,4 +1,23 @@
 import FitnessApp  from "./fitness.js";
 
 const app = document.getElementById("app");
-new FitnessApp(app);
+const wt = new FitnessApp(app);
+
+window.wt = wt;
+
+
+
+fetch("http://localhost:3000/images").then((data => {
+    return data.json();
+
+}).then ((completedata) => {
+
+    let data1="";
+    completedata.map((values) => {
+
+       data1=`<img src=${values.image}`   
+    });
+
+    document.getElementById("images").innerHTML = data1;
+
+}))
