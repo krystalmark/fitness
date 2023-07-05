0acfe87a91bf125810df9e471dcee4b2d91b8f40
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-07-06 00:51:59 +0300

    updated js

--- fitness project/index.js
+++ fitness project/index.js
@@ -1,4 +1,4 @@
-// Fetch fitness data from an API
+
 function fetchFitnessData() {
     fetch('http://localhost:3000/fitnesses')
       .then(response => response.json())
@@ -6,15 +6,15 @@ function fetchFitnessData() {
       .catch(error => console.error('Error:', error));
   }
   
-  // Display fitness data on the webpage
+
   function displayFitnessData(data) {
-    // Assuming the data is an array of fitness records with properties like 'date', 'steps', 'calories', etc.
+
     const fitnessContainer = document.getElementById('fitness-container');
   
-    // Clear previous data
+    
     fitnessContainer.innerHTML = '';
   
-    // Iterate through each fitness record and create HTML elements to display the data
+    
     data.forEach(record => {
       const recordElement = document.createElement('div');
       recordElement.innerHTML = `<p>Name: ${record.name}</p>
@@ -25,6 +25,6 @@ function fetchFitnessData() {
     });
   }
   
-  // Trigger data fetching and display on page load or any other event
+  
   fetchFitnessData();
   
