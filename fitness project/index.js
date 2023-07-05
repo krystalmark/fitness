0d97675dc6bdf2d3385317bbc1c470c7ff850933
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-07-05 15:28:25 +0300

    adding elements to html

--- fitness project/index.js
+++ fitness project/index.js
@@ -1,31 +1,29 @@
-import FitnessApp  from "./fitness.js";
-
-const app = document.getElementById("app");
-const wt = new FitnessApp(app);
-
-window.wt = wt;
-
-
-
-document.addEventListener("DOMContentLoaded", function(){
-    fetch("http://localhost:3000/fitness")
-     .then(response => response.json())
-     .then(data => {
-
-        const fitnessList = document.getElementById("fitness");
-        fitnessList.innerHTML = "";
-
-        data.forEach(fitness => {
-
-            constFitnessItem = document.createElement("li");
-            fitnessItem.classList.add("fitness-item");
-            fitnessItem.innerHTML = `
-            <p>Name: ${fitness.name}<p>
-            <p>Difficulty: ${fitness.difficulty}<P>
-            <p>Equipment: ${fitness.equipment}<p>
-            <p>instructions: ${fitness.instructions}<P>
-            `;
-            fitnessList.appendChild(fitnessItem);
-        });
-      });
-     });
+// Fetch fitness data from an API
+function fetchFitnessData() {
+    fetch('http://localhost:3000/fitnesses')
+      .then(response => response.json())
+      .then(data => displayFitnessData(data))
+      .catch(error => console.error('Error:', error));
+  }
+  
+  // Display fitness data on the webpage
+  function displayFitnessData(data) {
+    // Assuming the data is an array of fitness records with properties like 'date', 'steps', 'calories', etc.
+    const fitnessContainer = document.getElementById('fitness-container');
+  
+    // Clear previous data
+    fitnessContainer.innerHTML = '';
+  
+    // Iterate through each fitness record and create HTML elements to display the data
+    data.forEach(record => {
+      const recordElement = document.createElement('div');
+      recordElement.innerHTML = `<p>Date: ${record.date}</p>
+                                 <p>Steps: ${record.steps}</p>
+                                 <p>Calories: ${record.calories}</p>`;
+      fitnessContainer.appendChild(recordElement);
+    });
+  }
+  
+  // Trigger data fetching and display on page load or any other event
+  fetchFitnessData();
+  
