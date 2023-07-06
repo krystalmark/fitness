b461bca522f4f1ffb8749144134724c264850889
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-07-06 17:31:39 +0300

    updating Add entry span

--- fitness project/index.js
+++ fitness project/index.js
@@ -23,8 +23,48 @@ function fetchFitnessData() {
                                  <img src="${record.gifUrl}" alt="${record.title} GifUrl" />`;
       fitnessContainer.appendChild(recordElement);
     });
+
   }
-  
-  
+
   fetchFitnessData();
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const addEntryBtn = document.getElementById('addEntry');
+    const entryTable = document.getElementById('entryTable').getElementsByTagName('tbody')[0];
+    let entryCount = 1;
+  
+    addEntryBtn.addEventListener('click', function() {
+      const newEntry = document.createElement('tr');
+      const entryText = document.createElement('td');
+      entryText.textContent = 'Entry ' + (++entryCount);
+      const removeButtonCell = document.createElement('td');
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Times';
+      removeButton.className = 'removeBtn';
+      removeButton.addEventListener('click', function() {
+        entryTable.removeChild(newEntry);
+      });
+      removeButtonCell.appendChild(removeButton);
+      newEntry.appendChild(entryText);
+      newEntry.appendChild(removeButtonCell);
+      entryTable.appendChild(newEntry);
+    });
+  
+    document.addEventListener('click', function(event) {
+      if (event.target.classList.contains('removeBtn')) {
+        const entryRow = event.target.closest('tr');
+        if (entryRow) {
+          entryTable.removeChild(entryRow);
+        }
+      }
+    });
+  });
+  
+   
+  
+
+
+
+
   
