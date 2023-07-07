3abccd65a5b9bfaa60f4753ebc5d175e75ac779f
Krystal Mark <krystal.mark@student.moringaschool.com>
2023-07-07 15:21:13 +0300

    styling the entry table

--- fitness project/index.js
+++ fitness project/index.js
@@ -29,42 +29,54 @@ function fetchFitnessData() {
   fetchFitnessData();
 
 
+  
+   
+ 
   document.addEventListener('DOMContentLoaded', function() {
     const addEntryBtn = document.getElementById('addEntry');
     const entryTable = document.getElementById('entryTable').getElementsByTagName('tbody')[0];
     let entryCount = 1;
   
     addEntryBtn.addEventListener('click', function() {
-      const newEntry = document.createElement('tr');
-      const entryText = document.createElement('td');
-      entryText.textContent = 'Entry ' + (++entryCount);
-      const removeButtonCell = document.createElement('td');
-      const removeButton = document.createElement('button');
-      removeButton.textContent = 'Times';
-      removeButton.className = 'removeBtn';
-      removeButton.addEventListener('click', function() {
-        entryTable.removeChild(newEntry);
-      });
-      removeButtonCell.appendChild(removeButton);
-      newEntry.appendChild(entryText);
-      newEntry.appendChild(removeButtonCell);
-      entryTable.appendChild(newEntry);
+      const currentDate = new Date().toLocaleDateString();
+      const workout = prompt('Enter workout:');
+      const duration = prompt('Enter duration:');
+  
+      if (workout && duration) {
+        const newEntry = document.createElement('tr');
+        const dateCell = document.createElement('td');
+        const workoutCell = document.createElement('td');
+        const durationCell = document.createElement('td');
+        const removeButtonCell = document.createElement('td');
+        const removeButton = document.createElement('button');
+  
+        dateCell.textContent = currentDate;
+        workoutCell.textContent = workout;
+        durationCell.textContent = duration;
+        removeButton.textContent = 'Remove Entry';
+        removeButton.className = 'removeBtn';
+  
+        removeButton.addEventListener('click', function() {
+          entryTable.removeChild(newEntry);
+        });
+  
+        newEntry.appendChild(dateCell);
+        newEntry.appendChild(workoutCell);
+        newEntry.appendChild(durationCell);
+        newEntry.appendChild(removeButtonCell);
+        removeButtonCell.appendChild(removeButton);
+        entryTable.appendChild(newEntry);
+  
+        entryCount++;
+      }
     });
   
     document.addEventListener('click', function(event) {
       if (event.target.classList.contains('removeBtn')) {
         const entryRow = event.target.closest('tr');
         if (entryRow) {
-          entryTable.removeChild(entryRow);
+          entryRow.parentNode.removeChild(entryRow);
         }
       }
     });
   });
-  
-   
-  
-
-
-
-
-  
